5f9b0d4d4da74943b8f50b7753455fa844b6b609
ezeferex <ezequielferexco92@gmail.com>
2015-10-24 14:24:57 -0300

    --

---
+++ src/app.js
@@ -0,0 +1,9 @@
+(function(){
+	'use strict';
+
+	angular.module('StarterApp', ['ng-material'])
+		.controller('ListController', [function ($scope,$http) {
+			
+		}]);
+	angular.module('ng').filter('tel', function (){});
+})();
