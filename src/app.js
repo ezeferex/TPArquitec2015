5f8c20d2da0c91e2cbe9866095c1920a7fbf885a
ezeferex <ezequielferexco92@gmail.com>
2015-10-25 14:21:48 -0300

    ---

--- src/app.js
+++
@@ -1,9 +0,0 @@
-(function(){
-	'use strict';
-
-	angular.module('StarterApp', ['ng-material'])
-		.controller('ListController', [function ($scope,$http) {
-			
-		}]);
-	angular.module('ng').filter('tel', function (){});
-})();
---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+var app=angular.module('starterApp', [ 'ngMaterial' ]);
+app.controller('AppCtrl', ['$scope','$mdSidenav', function ($scope,$mdSidenav) {
+    $scope.toogleSideNav=function(menuId){
+        $mdSidenav(menuId).toggle()
+   .then(function(){
+   });
+    };
+}]);
