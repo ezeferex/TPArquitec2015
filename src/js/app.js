2a6aa130fc504fa5ea82ed6fa7b666e756419acc
ezeferex <ezequielferexco92@gmail.com>
2015-10-27 23:59:37 -0300

    Primer Commit
    Prototipo de interfaz resuelta del Home

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,9 @@ var app=angular.module('starterApp', [ 'ngMaterial' ]);
 app.controller('AppCtrl', ['$scope','$mdSidenav', function ($scope,$mdSidenav) {
     $scope.toogleSideNav=function(menuId){
         $mdSidenav(menuId).toggle()
-   .then(function(){
-   });
+   .then(function(){});
+  	$scope.signUp=function(){
+  		
+  	}
     };
 }]);
